c07802650d2924c29f50f9b22f1d1eb6a3f1c5bf
Hermes Francisco <franciscohermes2000@hotmail.com>
2021-06-11 21:13:46 -0300

    migration Ticket

---
+++ database/migrations/2021_06_11_235920_create_tickets_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTicketsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tickets', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained();
+            $table->text('description');
+            $table->foreignId('responsible_id')->constrained('users', 'id');
+            $table->integer('gravity');
+            $table->integer('urgency');
+            $table->integer('tendency');
+            $table->integer('priority');
+            $table->date('due');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tickets');
+    }
+}
